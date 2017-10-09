9dfe4770e8453cc5ff389db63830b404a3a9fc04
Elizabeth Hastings <elliehastings@gmail.com>
2017-10-09 16:08:35 -0400

    Get input box working and refactor container children css

--- application.js
+++ application.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
-	console.log('jQuery is working');
+	console.log('jQuery on');
 
 	/* Code below */
 	// Create divs
 	for (i=0; i<16*16; i++) {
-		$('.container').append('<div class="child-container" id="'+i+'"></div>');
+		$('.container').append('<div class="child-container child-container-default-size" id="'+i+'"></div>');
 	}
 
 	// Create hover effect
 	$(".child-container").hover(function() {
 		$(this).addClass('highlighted');
+	});
+
+	// Upon button press, get user input and reset grid
+	$('#header-button').click(function() {
+		var squares = prompt('How many squares would you like?','16');
+		console.log(squares);
 	});
 
 } );
